b40593f75cfe247b8122c8fb20e553e70bab90d9
xuhd <hdx@kland.com.cn>
2026-05-28 17:58:17 +0800

    feat: add react-router layout with Sidebar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
+import ChatPage from "./pages/ChatPage";
+import TaskListPage from "./pages/TaskListPage";
+import HistoryPage from "./pages/HistoryPage";
+
+export default function App() {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+        <Routes>
+          <Route path="/" element={<Navigate to="/chat" replace />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/tasks" element={<TaskListPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
